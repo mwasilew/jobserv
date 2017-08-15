08ce01982a6741c80631829687d042a8b092267e
Andy Doan <andy.doan@linaro.org>
2017-08-15 11:18:23 -0500

    stats: Add a runtime configurable stats API
    This can be configured to report to Carbon if desired.
    
    Signed-off-by: Andy Doan <andy.doan@linaro.org>

---
+++ jobserv/settings.py
@@ -0,0 +1,17 @@
+import os
+
+DEBUG = 1
+
+CARBON_HOST = os.environ.get('CARBON_HOST')
+if CARBON_HOST:
+    parts = CARBON_HOST.split(':')
+    if len(parts) == 1:
+        CARBON_HOST = (CARBON_HOST, 2003)  # provide default port
+    elif len(parts) == 2:
+        CARBON_HOST = (parts[0], int(parts[1]))
+    else:
+        raise ValueError('Invalid CARBON_HOST setting: ' + CARBON_HOST)
+
+CARBON_PREFIX = os.environ.get('CARBON_PREFIX', 'jobserv')
+if CARBON_PREFIX and CARBON_PREFIX[-1] != '.':
+    CARBON_PREFIX += '.'
---
+++ jobserv/stats.py
@@ -0,0 +1,39 @@
+import socket
+import time
+
+from jobserv.settings import CARBON_HOST, CARBON_PREFIX
+
+
+class CarbonClient(object):
+    def __init__(self):
+        self._sock = None
+        if CARBON_HOST:
+            self.send = self._real_send
+        else:
+            self.send = self._mock_send
+
+    def __enter__(self):
+        if CARBON_HOST:
+            self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._sock.connect(CARBON_HOST)
+        return self
+
+    def __exit__(self, *args):
+        if self._sock:
+            self._sock.close()
+
+    def _mock_send(self, metric, value, timestamp=None):
+        pass
+
+    def _real_send(self, metric, value, timestamp=None):
+        if timestamp is None:
+            timestamp = time.time()
+        buff = '%s%s %f %d\n' % (CARBON_PREFIX, metric, value, timestamp)
+        buff = buff.encode()
+        msglen = len(buff)
+        total = 0
+        while total < msglen:
+            sent = self._sock.send(buff[total:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            total += sent
