9f5a1333c6ae79293d86191f67b8f5755e623674
Andy Doan <andy.doan@linaro.org>
2017-08-15 15:43:47 -0500

    api: Add promoted builds api
    Signed-off-by: Andy Doan <andy.doan@linaro.org>

--- jobserv/api/build.py
+++ jobserv/api/build.py
@@ -2,22 +2,24 @@ from flask import Blueprint, request, url_for
 
 from jobserv.storage import Storage
 from jobserv.internal_requests import internal_api
-from jobserv.jsend import get_or_404, jsendify, paginate
-from jobserv.models import Build, BuildStatus, Project
+from jobserv.jsend import (
+    ApiError, get_or_404, jsendify, paginate, paginate_custom
+)
+from jobserv.models import Build, BuildStatus, Project, db
 from jobserv.trigger import trigger_build
 
 blueprint = Blueprint(
-    'api_build', __name__, url_prefix='/projects/<proj>/builds')
+    'api_build', __name__, url_prefix='/projects/<proj>')
 
 
-@blueprint.route('/', methods=('GET',))
+@blueprint.route('/builds/', methods=('GET',))
 def build_list(proj):
     p = get_or_404(Project.query.filter(Project.name == proj))
     q = Build.query.filter_by(proj_id=p.id).order_by(Build.id.desc())
     return paginate('builds', q)
 
 
-@blueprint.route('/', methods=('POST',))
+@blueprint.route('/builds/', methods=('POST',))
 @internal_api
 def build_create(proj):
     p = Project.query.filter(Project.name == proj).first_or_404()
@@ -30,7 +32,7 @@ def build_create(proj):
     return jsendify({'url': url}, 201)
 
 
-@blueprint.route('/<int:build_id>/', methods=('GET',))
+@blueprint.route('/builds/<int:build_id>/', methods=('GET',))
 def build_get(proj, build_id):
     p = get_or_404(Project.query.filter(Project.name == proj))
     b = get_or_404(
@@ -38,7 +40,7 @@ def build_get(proj, build_id):
     return jsendify({'build': b.as_json(detailed=True)})
 
 
-@blueprint.route('/<int:build_id>/project.yml', methods=('GET',))
+@blueprint.route('/builds/<int:build_id>/project.yml', methods=('GET',))
 def build_get_project_definition(proj, build_id):
     p = get_or_404(Project.query.filter(Project.name == proj))
     b = get_or_404(
@@ -47,7 +49,7 @@ def build_get_project_definition(proj, build_id):
     return pd, 200, {'Content-Type': 'text/yaml'}
 
 
-@blueprint.route('/latest/', methods=('GET',))
+@blueprint.route('/builds/latest/', methods=('GET',))
 def build_get_latest(proj):
     '''Return the most recent successful build'''
     b = get_or_404(
@@ -61,3 +63,60 @@ def build_get_latest(proj):
         )
     )
     return jsendify({'build': b.as_json(detailed=True)})
+
+
+@blueprint.route('/builds/<int:build_id>/promote', methods=('POST',))
+@internal_api
+def build_promote(proj, build_id):
+    p = get_or_404(Project.query.filter_by(name=proj))
+    b = get_or_404(Build.query.filter_by(project=p, build_id=build_id))
+
+    if not b.complete:
+        raise ApiError(400, 'Build is not yet complete')
+
+    data = request.get_json()
+    if not data:
+        raise ApiError(400, 'Input data must be JSON')
+
+    b.status = BuildStatus.PROMOTED
+    b.name = data.get('name')
+    b.annotation = data.get('annotation')
+    db.session.commit()
+    return jsendify({}, 201)
+
+
+def _promoted_as_json(storage, build):
+    rv = build.as_json(detailed=True)
+    rv['tests'] = []
+    rv['artifacts'] = []
+    for run in build.runs:
+        for t in run.tests:
+            test = t.as_json(detailed=True)
+            test['name'] = '%s-%s' % (run.name, test['name'])
+            rv['tests'].append(test)
+        for a in storage.list_artifacts(run):
+            rv['artifacts'].append('%s/%s' % (run.name, a))
+    return rv
+
+
+@blueprint.route('/builds/promoted-builds/', methods=('GET',))
+def promoted_build_list(proj):
+    p = get_or_404(Project.query.filter_by(name=proj))
+    q = Build.query.filter(
+        Build.proj_id == p.id
+    ).filter(
+        Build.status == BuildStatus.PROMOTED
+    ).order_by(Build.id.desc())
+
+    s = Storage()
+    return paginate_custom('builds', q, lambda x: _promoted_as_json(s, x))
+
+
+@blueprint.route('/builds/promoted-builds/<name>/', methods=('GET',))
+def promoted_build_get(proj, name):
+    b = get_or_404(Build.query.join(Project).filter(
+        Project.name == proj,
+        Build.status == BuildStatus.PROMOTED,
+        Build.name == name,
+        ))
+    return jsendify({'build': _promoted_as_json(Storage(), b)})
--- tests/test_api_build.py
+++ tests/test_api_build.py
@@ -3,7 +3,7 @@ import json
 from unittest.mock import patch
 
 from jobserv.internal_requests import _sign
-from jobserv.models import Build, BuildStatus, Project
+from jobserv.models import Build, BuildStatus, Project, Run, Test, db
 
 from tests import JobServTest
 
@@ -14,6 +14,11 @@ class BuildAPITest(JobServTest):
         self.create_projects('proj-1')
         self.project = Project.query.all()[0]
         self.urlbase = '/projects/%s/builds/' % self.project.name
+
+    def _post(self, url, data, headers, status=200):
+        resp = self.client.post(url, data=data, headers=headers)
+        self.assertEqual(status, resp.status_code, resp.data)
+        return resp
 
     def test_no_builds(self):
         builds = self.get_json(self.urlbase)['builds']
@@ -48,11 +53,11 @@ class BuildAPITest(JobServTest):
         self.assertEqual(
             ['QUEUED'], [x['status'] for x in data['status_events']])
 
-    @patch('jobserv.api.Storage')
+    @patch('jobserv.api.build.Storage')
     def test_build_get_definition(self, storage):
         Build.create(self.project)
         storage().get_project_defintion.return_value = 'foo: bar'
-        r = self.client.get(self.urlbase + 'job-1/builds/1/project.yml')
+        r = self.client.get(self.urlbase + '1/project.yml')
         self.assertEqual(200, r.status_code, r.data)
         self.assertEqual('foo: bar', r.data.decode())
 
@@ -77,3 +82,75 @@ class BuildAPITest(JobServTest):
         data = json.loads(r.data.decode())
         self.assertEqual('error', data['status'])
         self.assertIn('console.log', r.headers['Location'])
+
+    @patch('jobserv.api.build.Storage')
+    def test_promote_list_empty(self, storage):
+        b = Build.create(self.project)
+        db.session.add(Run(b, 'run0'))
+        db.session.add(Run(b, 'run1'))
+        for r in b.runs:
+            r.set_status(BuildStatus.PASSED)
+        builds = self.get_json(self.urlbase + 'promoted-builds/')['builds']
+        self.assertEqual(0, len(builds))
+
+    @patch('jobserv.api.build.Storage')
+    def test_promote_list(self, storage):
+        b = Build.create(self.project)
+        db.session.add(Run(b, 'run0'))
+        db.session.add(Run(b, 'run1'))
+        for r in b.runs:
+            r.set_status(BuildStatus.PASSED)
+            t = Test(r, 't1', None, BuildStatus.PASSED)
+            db.session.add(t)
+        b.status = BuildStatus.PROMOTED
+        b.name = 'release-X'
+        b.annotation = 'foo bar'
+        builds = self.get_json(self.urlbase + 'promoted-builds/')['builds']
+        self.assertEqual(1, len(builds))
+        self.assertEqual('release-X', builds[0]['name'])
+        self.assertEqual('foo bar', builds[0]['annotation'])
+        self.assertEqual(
+            ['run0-t1', 'run1-t1'], [x['name'] for x in builds[0]['tests']])
+
+    @patch('jobserv.api.build.Storage')
+    def test_promote_get(self, storage):
+        b = Build.create(self.project)
+        db.session.add(Run(b, 'run0'))
+        db.session.add(Run(b, 'run1'))
+        for r in b.runs:
+            r.set_status(BuildStatus.PASSED)
+            t = Test(r, 't1', None, BuildStatus.PASSED)
+            db.session.add(t)
+        b.status = BuildStatus.PROMOTED
+        b.name = 'release-X'
+        b.annotation = 'foo bar'
+        build = self.get_json(
+            self.urlbase + 'promoted-builds/release-X/')['build']
+        self.assertEqual('foo bar', build['annotation'])
+
+    def test_promote_post(self):
+        b = Build.create(self.project)
+        db.session.add(Run(b, 'run0'))
+        db.session.add(Run(b, 'run1'))
+
+        url = 'http://localhost/projects/proj-1/builds/%d/promote' % b.build_id
+
+        headers = {
+            'Content-type': 'application/json',
+        }
+        data = {
+            'name': 'release-x',
+            'annotation': 'foo bar',
+        }
+
+        # you can't promote an in-progress build
+        _sign(url, headers, 'POST')
+        self._post(url, json.dumps(data), headers, 400)
+
+        for r in b.runs:
+            r.set_status(BuildStatus.PASSED)
+        self._post(url, json.dumps(data), headers, 201)
+        db.session.refresh(b)
+        self.assertEqual(BuildStatus.PROMOTED, b.status)
+        self.assertEqual(data['name'], b.name)
+        self.assertEqual(data['annotation'], b.annotation)
