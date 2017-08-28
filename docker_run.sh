55dc03af4c52942e4105c3fcc07c4e1e36363b7c
Andy Doan <andy.doan@linaro.org>
2017-08-28 16:47:44 -0500

    DB: Add automatic migration logic
    If FLASK_AUTO_MIGRATE is specified an NFS friendly lock will be made
    on that file and we'll automatically run alembic database migrations
    when a container is started.
    
    Signed-off-by: Andy Doan <andy.doan@linaro.org>

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,29 @@
 #!/bin/sh -e
 
+create_flock_script () {
+	# /usr/bin/flock uses the "flock" call instead of fcntl.
+	# fcntl is required for NFS shares
+	cat > /tmp/flock <<EOF
+#!/usr/bin/python3
+import fcntl
+import subprocess
+import sys
+
+
+with open(sys.argv[1], 'a') as f:
+    fcntl.flock(f, fcntl.LOCK_EX)
+    subprocess.check_call(sys.argv[2:])
+EOF
+chmod +x /tmp/flock
+}
+
+if [ -n "$FLASK_AUTO_MIGRATE" ] ; then
+	create_flock_script
+	echo "Peforming DB migration"
+	mkdir -p $(dirname $FLASK_AUTO_MIGRATE)
+	/tmp/flock $FLASK_AUTO_MIGRATE flask db upgrade
+fi
+
 # if FLASK_DEBUG is defined, we'll run via flask with dynamic reloading of
 # code changes to disk. This is helpful for debugging something already in k8s
 
