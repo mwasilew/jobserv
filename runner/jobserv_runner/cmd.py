24f6a4894fbbb0847f1d8aabaac756dac3017108
Andy Doan <andy@foundries.io>
2020-02-20 22:13:42 -0600

    runner: Add some debug to stream_cmd
    This attempts to detect a "hung" command - ie it hasn't seen any data in
    over 10 minutes.
    
    It also tries to log unexpected poll events we might get.
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- runner/jobserv_runner/cmd.py
+++ runner/jobserv_runner/cmd.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2017 Linaro Limited
 # Author: Andy Doan <andy.doan@linaro.org>
 
+import datetime
 import os
 import select
 import subprocess
@@ -20,13 +21,28 @@ def _cmd_output(cmd, cwd=None, env=None):
     for fd in fds:
         poller.register(fd, select.POLLIN)
 
+    timeouts = 0
     while len(fds) > 0:
-        for fd, event in poller.poll(1000):
+        ready = poller.poll(1000)
+        if len(ready) > 0:
+            timeouts = 0
+        else:
+            timeouts += 1
+            if timeouts == 300:
+                # we've gone 5 minutes without output
+                msg = '== %s: cmd seems hung\n' % datetime.datetime.utcnow()
+                yield msg.encode()
+                timeouts = 0  # give a chance to warn again in another 5
+        for fd, event in ready:
             if event & select.POLLIN:
                 yield os.read(fd, 1024)
             elif event & select.POLLHUP:
                 poller.unregister(fd)
                 fds.remove(fd)
+            else:
+                msg = '== %s: unexpected poll event\n' % (
+                    datetime.datetime.utcnow(), event)
+                yield msg.encode()
     p.wait()
     p.stdout.close()
     if p.returncode != 0:
